fc7213459845ff3de2780036d8f2423bd5e96885
gdub <dotvenv@skiff.com>
2024-08-15 19:39:48 -0500

    inrprogress: animation

--- gb2/gb/static/gb/static/reactjs/components/AnimationTest/animationtest.js
+++ gb2/gb/static/gb/static/reactjs/components/AnimationTest/animationtest.js
@@ -6,16 +6,11 @@ const AnimationTest = () => {
   return (
     <div id="canvas-container" className="justify-center align-center mx-auto">
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <Sphere
-          args={[1, 16, 16]}
-          position={[14, 12, 3]}
-          rotation={[Math.PI / 2, 0, 0]}
-        >
-         
-        </Sphere>
         <OrbitControls />
+        <mesh>
+            <boxGeometry />
+            <meshBasicMaterial />
+        </mesh>
       </Canvas>
     </div>
   );
