a7c0446f03d020cb014f442409a2efec3288e614
gdub <dotvenv@skiff.com>
2024-08-16 15:18:33 -0500

    iP: ui

--- gb2/gb/static/gb/static/reactjs/components/Animations/che_holder.js
+++ gb2/gb/static/gb/static/reactjs/components/Animations/che_holder.js
@@ -1,27 +1,30 @@
 import React, { Suspense, useEffect, useRef } from "react";
 
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PivotControls } from "@react-three/drei";
 import { motion } from "framer-motion";
 import { MotionConfig } from "framer-motion";
 import { CheModel } from "./che";
-import { Canvas } from "@react-three/fiber";
-
+import { Canvas, useFrame } from "@react-three/fiber";
+import { PerspectiveCamera } from "three";
 
 
 const CheAnim = ({children}) => {
-
+    
     return(
         <>
-            <div className=' relative'>
+            <div className=' relative h-[550px]'>
                 <div className='absolute z-10 inset-0'>
                     {children}
                 </div>
                 <Suspense fallback ={null}>
-                    <Canvas 
-                        className='top-0 z-0 inset-0 h-[550px] justify-center opacity-15 align-center mx-auto'>
-                        <CheModel />
-                        <OrbitControls  enableZoom={false} />
-                    </Canvas>
+                    <motion.div
+                        scale={2}
+                        initial={{x:50, y:50}}>
+                        <Canvas className='top-0 z-0 inset-0 h-full justify-center opacity-15 align-center mx-auto'>
+                            <CheModel />
+                            <OrbitControls />
+                        </Canvas>
+                    </motion.div>
                 </Suspense>
             </div>
         </>
