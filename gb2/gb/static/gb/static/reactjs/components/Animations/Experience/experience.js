96a339d1c57215ee67fb2d1a041b35bfcf701453
gdub <dotvenv@skiff.com>
2024-08-19 16:20:15 -0500

    inp: Ui

--- gb2/gb/static/gb/static/reactjs/components/Animations/Experience/experience.js
+++ gb2/gb/static/gb/static/reactjs/components/Animations/Experience/experience.js
@@ -1,12 +1,8 @@
 import React, { useRef } from "react";
 
-import { Canvas } from "@react-three/fiber";
-import { Capsule, OrbitControls, PerspectiveCamera, Html, useScroll, Environment, PresentationControls, PivotControls, ScrollControls } from "@react-three/drei";
-import WatcherModel from "../Room/Watcher";
 import DescriptionSplit from "../../DescriptSplit/descriptsplit";
-import { MotionCanvas } from "framer-motion-3d";
 import { motion } from "framer-motion";
-
+import { Canvas } from "@react-three/fiber";
 const Experience = ({children}) =>{ 
     return(
             <> 
@@ -20,23 +16,7 @@ const Experience = ({children}) =>{
                         <motion.div
                             animate={{x:360}}
                         >
-                            <Canvas>           
-                                <Environment preset="sunset" />
-                                <PerspectiveCamera makeDefault fov={4} position={[0, 0, 50]} />
-                                
-                                <mesh castShadow>
-                                    <ambientLight intensity={2} />
-                                    <pointLight position={[0, 0, 1.949]} />
-                                   
-                                     <WatcherModel />
-                                    
-                                    <OrbitControls 
-                                        enableZoom={false}
-                                        maxPolarAngle={5}
-                                        maxAzimuthAngle={0.5}
-                                    />
-                                </mesh>
-                            </Canvas>
+                            
                         </motion.div>
                      
                     
