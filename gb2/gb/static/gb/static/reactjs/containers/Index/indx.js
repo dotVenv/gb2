967f6d9130a257daebfb86797aa59435de788749
gdub <dotvenv@skiff.com>
2024-08-28 19:51:39 -0500

    ui changes

--- gb2/gb/static/gb/static/reactjs/containers/Index/indx.js
+++ gb2/gb/static/gb/static/reactjs/containers/Index/indx.js
@@ -8,30 +8,37 @@ import {
     DescriptionSplit,
     Experience,
     BorderBeam,
-    ShineBorder,
-    RetroGrid,
-    AnimatedGradientText,
    
     } from "../../components";
 
-import { cn,  Card, CardBody, Spacer, Button } from "@nextui-org/react";
+import { cn,  Card, CardBody, CardHeader, CardFooter, Spacer, Button, Chip } from "@nextui-org/react";
 import { motion } from "framer-motion";
 
 
 
 const tourney_journey = [
     {
-      name: 'Push to deploy',
+      name: 'Real-Time Ranking',
       description:
-        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+        'When it comes to accuracy we nail it by blocking late tournament entries and updating leaderboards very frequently (3s) to ensure the latest stats. We also heavily monitor the data returned from each match to ensure correct game results. Players flagged as cheat-risks will have to download "Gamers-Bounty\'s GCheck-TTK" (GameCheck Task-To-Kernel) to ensure no cheating on PC  is accuring during the tournament.',
       icon: null,
     },
     {
-      name: 'SSL certificates',
-      description:
-        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-      icon: null,
+        name: 'Real-Time Monitoring',
+        description:'',
+        icon: null,
     },
+    {
+        name: 'Player vs Player Chat',
+        description:'',
+        icon: null,
+    },
+    {
+        name: 'Same Day Payouts',
+        description:'',
+        icon: null,
+    },
+ 
 
 ]
 const CustomButton = ({children}) => {
@@ -71,7 +78,7 @@ const Indx = () => {
                     </section>
 
                     <br></br>
-                    <section className="relative h-[115vh] mb-4 pb-4">
+                    <section className="relative h-[105vh] mb-4 pb-4">
                         <br></br>
                         <Spacer></Spacer>
                         <div>
@@ -87,57 +94,35 @@ const Indx = () => {
                     <section className='mt-4 pt-4'>
                         <DP />
                         <Spacer></Spacer>
-                        <motion.div
-                            initial={{
-                                x: -175,
-                                y: 150,
-                                scale: 0.1,
-                                rotate: 0,
-                                }}
-                            whileInView={{
-                                x: 0,
-                                y: 150,
-                                scale: 1.0,
-                            }}
-                            viewport={{once:true}}
-                            >
-                            <AnimatedGradientText className='justify-center align-center mx-auto col-8 bg-white'>
-                                <div className="bg-white py-24 sm:py-32">
-                                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                                        <div className="mx-auto max-w-2xl lg:text-center">
-                                        <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                        
+                            <Card isBlurred className='justify-center align-center mx-auto flex-cols col-9 bg-white h-full'>
+                                <CardHeader>
+                                    <div className="mx-auto mt-16  sm:mt-20 lg:mt-24 lg:text-center">
+                                        <h2 className="text-base font-semibold leading-7 text-indigo-600">Start Competing Now</h2>
                                         <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                                            Everything you need to deploy your app
+                                            Simply register and enter a tournament.
                                         </p>
-                                        <p className="mt-6 text-lg leading-8 text-gray-600">
-                                            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-                                            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                                        <p className="mt-6 text-tiny leading-2 text-gray-600">
+                                            US Gamers must reside in <i> a valid state that allows online gaming competitions. </i>
                                         </p>
-                                        </div>
-                                        <div className='row '>
-                                            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                                            
-                                            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-1 lg:gap-y-16">
-                                                {tourney_journey.map((feature) => (
-                                                <div key={feature.name} className="relative pl-16">
-                                                    <dt className="text-base font-semibold leading-7 text-gray-900">
-                                                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                                                        
-                                                    </div>
-                                                    {feature.name}
-                                                    </dt>
-                                                    <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-                                                </div>
-                                                ))}
-                                            </dl>
-                                            </div>
+                                    </div>
+                                </CardHeader>
+                                    <CardBody className='w-full h-full mb-4'>
+                                    
+                                        <div className="mx-auto ">
                                             <TourneyJourney />
                                         </div>
-                                    </div>
-                                </div>
-                                <RetroGrid />
-                            </AnimatedGradientText>
-                        </motion.div>
+                                        <br></br>
+                                        <div className="justify-center align-center items-center mx-auto lg:flex sm:grid sm:grid-cols-1 gap-y-2">
+                                            {tourney_journey.map((feature) => (
+                                                <Chip variant='flat' size='md' color='success' className='font-bold text-small justify-center align-center mb-2'>
+                                                    {feature.name}
+                                                </Chip>
+                                            ))}
+                                        </div>
+                                        </CardBody>
+                                <BorderBeam />
+                            </Card>
           
                     </section>
                 </Experience>
--- gb2/gb/static/gb/static/reactjs/containers/sections/tourneyjourney.js
+++ gb2/gb/static/gb/static/reactjs/containers/sections/tourneyjourney.js
@@ -8,15 +8,11 @@ import { Card } from "@nextui-org/react";
 const TourneyJourney = () => {
 
     return (
-       
-   
-            <div className="relative flex h-[450px]  w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-foreground md:shadow-xl">
-
-                <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-gray dark:to-slate-900/10">
-                        <EasySteps />
-                </span>
-            </div>
-            
+        <div className='justify-center align-center mx-auto'>
+            <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-gray dark:to-slate-900/10">
+                <EasySteps />
+            </span>
+        </div>
        
     );
 };
