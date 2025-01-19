f660069ec8bae9e1327881f68ee66f45c034e1ba
ceog <dev@venv.pro>
2025-01-19 10:49:05 -0600

    init gb_app

---
+++ gb2/gb_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+uiviews = views.UIViews()
+
+urlpatterns = [
+    path('', uiviews.index, name='dashboard'),
+
+]
--- gb2/gb_app/views.py
+++ gb2/gb_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#djano imports
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class UIViews(TemplateView):
+    
+    def dashboard(self, request):
+        
+        context = {}
+        return render(request, 'gb_app/templates/index.html', context)
