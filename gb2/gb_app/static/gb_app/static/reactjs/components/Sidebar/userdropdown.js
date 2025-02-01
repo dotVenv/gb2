a53db8d261a86d09de1a3a6d3c016b5ef052023e
ceog <dev@venv.pro>
2025-02-01 13:04:11 -0600

    wip: win loss stats

--- gb2/gb_app/static/gb_app/static/reactjs/components/Sidebar/userdropdown.js
+++ gb2/gb_app/static/gb_app/static/reactjs/components/Sidebar/userdropdown.js
@@ -1,6 +1,6 @@
 'use client';
 import React, {useContext} from "react";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem,Avatar } from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, Link, DropdownItem,Avatar } from "@nextui-org/react";
 import { ConnContext } from "../../connector";
 
 const UserDropdown = ({ userInfo }) => {
@@ -9,7 +9,7 @@ const UserDropdown = ({ userInfo }) => {
     return(
         <>
         
-        <Dropdown placement="bottom-end" className='text-white bg-zinc-800 items-center rounded-large rounded-border' radius='lg' size='sm'>
+        <Dropdown placement="bottom-end" className=' bg-zinc-800 items-center rounded-large rounded-border' radius='lg' size='sm'>
         <DropdownTrigger>
             <Avatar
             
@@ -23,14 +23,17 @@ const UserDropdown = ({ userInfo }) => {
                 src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
             />
         </DropdownTrigger>
-        <DropdownMenu aria-label="User Dropdown"  className='p-2 mx-auto text-gray-400' radius='lg'  variant="faded">
+        <DropdownMenu aria-label="User Dropdown"  className='p-2 mx-auto text-gray-400' radius='lg'  variant="faded"
+            disabledKeys={["profile"]}
+           
+           >
             <DropdownItem key="profile" className="h-14 gap-0 mt-3  p-3">
             <p className="font-bold">Signed in @{ userInfo.username }</p>
           </DropdownItem>
-          <DropdownItem key="settings" color='primary'><a underline='none'href='/myprofile'>My Profile</a></DropdownItem>
-          <DropdownItem key="team_settings" color='primary'><a underline='none'href='/entries'>My Entries ({ userInfo.entries })</a></DropdownItem>
-          <DropdownItem key="team_settings" color='primary'><a  underline='none' href='/myteam'>My Team</a></DropdownItem>
-          <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+          <DropdownItem key="settings" onPress={(e) => {location.href='/myprofile' }}  color='success'>My Profile</DropdownItem>
+          <DropdownItem key="team_settings" onPress={(e) => {location.href='/entries' }}  color='success'>My Entries ({ userInfo.entries })</DropdownItem>
+          <DropdownItem key="team_settings" onPress={(e) => {location.href='/myteam' }}  color='success'>My Team</DropdownItem>
+          <DropdownItem onPress={(e) => {location.href='/help' }} key="help_and_feedback" color='warning'>Help & Feedback</DropdownItem>
           <DropdownItem key="logout" href='/logout' color="danger">
             Log Out
           </DropdownItem>
