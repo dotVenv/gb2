f5b2716239b22ecf5e8ad51b516d343d96ecee0c
ceog <dev@venv.pro>
2025-01-31 22:44:44 -0600

    wip: dashboard UI

--- gb2/gb_app/static/gb_app/static/reactjs/components/StickyOptions/stickyoptions.js
+++ gb2/gb_app/static/gb_app/static/reactjs/components/StickyOptions/stickyoptions.js
@@ -6,7 +6,7 @@ import { Tabs, Tab } from "@nextui-org/react";
 const StickyOptions  = () => {
     return(
         <>
-            <Tabs aria-label="Options" className='fixed  left-25 bottom-1 ' isVertical={false} color="secondary" variant="bordered">
+            <Tabs aria-label="Options" className='fixed  justify-center align-center mx-autoleft-25 bottom-10 ' isVertical={false} color="secondary" variant="bordered">
         
             <Tab
                 key="holder"
