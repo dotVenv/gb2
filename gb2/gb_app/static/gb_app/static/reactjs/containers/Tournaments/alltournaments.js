cadcfcbee22c4a3e6ad92cef911071bf4c93137d
ceog <dev@venv.pro>
2025-02-15 02:18:22 -0600

    wip: search/filter tournaments

--- gb2/gb_app/static/gb_app/static/reactjs/containers/Tournaments/alltournaments.js
+++ gb2/gb_app/static/gb_app/static/reactjs/containers/Tournaments/alltournaments.js
@@ -4,7 +4,7 @@ import React, { useContext, useEffect } from 'react';
 import { ConnContext } from "../../connector";
 import { useAtom } from "jotai";
 import { Layout } from '../index';
-import { Breadcrumbs, BreadcrumbItem, Card,  CardBody, CardFooter, Spacer, Button, Chip, Image } from "@nextui-org/react";
+import { Breadcrumbs, BreadcrumbItem, Select, SelectItem, Spacer, Button, Input } from "@nextui-org/react";
 import { TournamentList } from '../../components';
 import { signal } from '@preact/signals-react';
 
@@ -43,6 +43,23 @@ const AllTournaments = () => {
                 </section>
             <section className='h-full'>
                 <div className='mt-4 py-4 col-9 justify-center align-center mx-auto'>
+                    <div className='flex'>
+                        <Input className='col-4 bg-zinc-700' radius='lg' size='lg' 
+                            endContent={<Button size='sm' color='default' isIconOnly radius='lg'><i className='fa-solid fa-search'></i></Button>} 
+                            placeholder='Search tournaments' />
+                            <Spacer></Spacer>
+                            <Select
+                                className="text-white max-w-xs col-3"
+                                items={TournamentFilters}
+                                size='sm'
+                                radius='lg'
+                                label="Filter by Title"
+                                >
+                                {(tourney) => <SelectItem className='text-white'>{tourney.name}</SelectItem>}
+                                </Select>
+                     
+                    </div>
+                    <br></br>
                     <div className="justify-center align-center mx-auto grid sm:grid-cols-1 lg:grid-cols-3 gap-0 mb-4">
                         <TournamentList cu={cu} />
                       
@@ -56,3 +73,8 @@ const AllTournaments = () => {
 };
 
 export default  AllTournaments;
+
+
+
+
+const TournamentFilters = [{'key':'nba2k','name':'NBA2K'}, {'key':'madden','name':'MADDEN'}, {'key':'rivals','name':'Rivals'}]
